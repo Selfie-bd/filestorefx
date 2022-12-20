8ced7ae11cf5a3b732b62816907410cf75c570e1
GreyMatter's <greymattersyt@gmail.com>
2022-12-20 08:50:46 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Owner : <a href='https://telegram.dog/GreyMatter_Owner'>GreyMatter's Owner</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/Greymattersbot/File-Sharing-Bot'>Click here</a>\n○ Channel : @GreyMatter_Bots\n○ Support Group : @GreyMatters_Bots_Discussion</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
